4e675b48cd4f475170140bf30958eefdd340fc68
silviafds <silviafernandes974@gmail.com>
2023-02-14 11:51:07 -0300

    usando padrao dto para retornar usuarios

---
+++ src/main/java/com/workshopmongo/work/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.workshopmongo.work.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.workshopmongo.work.domain.User;
+
+public class UserDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+		
+	}
+
+	public UserDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+
+}
