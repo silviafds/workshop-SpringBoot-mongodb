6e1da2dc8ff2c467197b22cfde3b0f82a76989ee
silviafds <silviafernandes974@gmail.com>
2023-02-15 12:03:11 -0300

    retorna usuario pelo id

---
+++ src/main/java/com/workshopmongo/work/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.workshopmongo.work.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	
+
+}
